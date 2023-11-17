8aca4090158318b37518d489367cbfcd2378b832
Charly0400 <Carlaz020400@gmail.com>
2023-11-17 11:07:00 -0600

    Request
    Se hizo el request con Glitch

---
+++ NetworController.gd
@@ -0,0 +1,17 @@
+extends Node
+
+@export var user: String
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	$HTTPRequest.request_completed.connect(_onComplete)
+	$HTTPRequest.request("https://silicon-exuberant-cantaloupe.glitch.me/joinServer?name="+user)
+	
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _onComplete (result, response_code, headres, body):
+	print(body.get_string_from_utf8())
+
+func _GetToID(String):
+	pass
+	
