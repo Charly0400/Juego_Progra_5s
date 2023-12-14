af30e8a2ce8147b21f4b306274f8cc9f9262304a
Charly0400 <Carlaz020400@gmail.com>
2023-12-14 16:34:33 -0500

    111
    asd

--- NetworController.gd
+++ NetworController.gd
@@ -1,7 +1,14 @@
 extends Node
 
 @export var user: String
+var ServerPlayerID : int
+var PlayerConnected : bool 
+var button : Button
 # Called when the node enters the scene tree for the first time.
+func _process(delta):
+	if PlayerConnected == true:
+		$HTTPRequest.request("https://silicon-exuberant-cantaloupe.glitch.me/joinRoom?id=" + str(ServerPlayerID))
+
 func _ready():
 	$HTTPRequest.request_completed.connect(_onComplete)
 	$HTTPRequest.request("https://silicon-exuberant-cantaloupe.glitch.me/joinServer?name="+user)
@@ -10,6 +17,14 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _onComplete (result, response_code, headers, body):
+	print(body.get_string_from_utf8())
+	var JSONRes = JSON.parse_string(body.get_string_from_utf8())
+	print(JSONRes.id)
+	PlayerConnected = true
+	$HTTPRequest.request_completed.disconnected(_onComplete)
+	$HTTPRequest.request_completed.connect(_OnCompJoinRoom)
+	
+func _OnCompJoinRoom (result, response_code, headers, body):
 	print(body.get_string_from_utf8())
 
 func _GetToID(String):
