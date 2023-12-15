1370a0f9e55b290708417137d019e34a6b8c00c3
Charly0400 <Carlaz020400@gmail.com>
2023-12-15 13:39:02 -0500

    entrega

--- NetworkController.gd
+++ NetworkController.gd
@@ -3,30 +3,34 @@ extends Node
 @export var user: String
 var ServerPlayerID : int
 var PlayerConnected : bool 
-var button : Button
+var request_in_progress: bool
+
 # Called when the node enters the scene tree for the first time.
 func _process(delta):
-	if PlayerConnected == true:
+	if PlayerConnected and not request_in_progress:
+		request_in_progress = true
 		$HTTPRequest.request("https://silicon-exuberant-cantaloupe.glitch.me/joinRoom?id=" + str(ServerPlayerID))
 
+
 func _ready():
+	PlayerConnected = false
+	request_in_progress = false
 	$HTTPRequest.request_completed.connect(_onComplete)
 	$HTTPRequest.request("https://silicon-exuberant-cantaloupe.glitch.me/joinServer?name="+user)
 	
-
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _onComplete (result, response_code, headers, body):
 	print(body.get_string_from_utf8())
 	var JSONRes = JSON.parse_string(body.get_string_from_utf8())
 	print(JSONRes.id)
+	ServerPlayerID = JSONRes.id
 	PlayerConnected = true
-	$HTTPRequest.request_completed.disconnected(_onComplete)
-	$HTTPRequest.request_completed.connect(_OnCompJoinRoom)
+	request_in_progress = false  # Marcar la solicitud como completada
+	$HTTPRequest.request_completed.disconnect(_onComplete)
+	$HTTPRequest.request_completed.connect(_OnCompJoinPlayRoom)
 	
-func _OnCompJoinRoom (result, response_code, headers, body):
+func _OnCompJoinPlayRoom (result, response_code, headers, body):
 	print(body.get_string_from_utf8())
 
 func _GetToID(String):
 	pass
-
